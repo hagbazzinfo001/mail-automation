4de493b0beb42f6e2cf2e893bbb165326b1c1aed
hagbazzinfo001 <agbabiakahammed003@gmail.com>
2025-10-21 10:21:20 +0100

    hfd

---
+++ memory_log.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+
+#DATE=`date`
+#date2=$(date) #---this also is another way
+
+set -e #EXIT IMMEDIATELY IF THERE'S ANY ERROR OF ANY LINES
+
+
+# Configurable variables
+LOGFILE="/home/vagrant/ram_usage.log"
+EMAIL="Agbabiakahammed003@gmail.com"
+
+# Get current time
+HOUR=$(date +%H)
+DATE=$(date +%Y-%m-%d)
+
+# Log RAM usage (free -m gives memory in MB, grep filters, awk extracts)
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] RAM Usage: $(free -m | awk '/Mem:/ {print $3 "MB used / " $2 "MB total"}')" >> $LOGFILE
+
+# If it's midnight (hour == 00)
+if [ "$HOUR" -eq "00" ]; then
+	    # Send log file to email
+	        mail -s "Daily RAM Usage Report ($DATE)" "$EMAIL" < $LOGFILE
+
+		    # Clear log file for new day
+		        > $LOGFILE
+fi
